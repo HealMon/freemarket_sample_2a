becc1b6b98dc38f86a32118fdedf861a1fac0ba9
masuteru <k.kishimoto50@gmail.com>
2019-10-03 17:07:04 +0900

    login, signup, signup/registrationページの作成
    BEMを使用

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
 Rails.application.routes.draw do
-  devise_for :users
+  
+  devise_for :users, skip: :all
+  devise_scope :user do
+    get 'signup/registration/' => 'devise/registrations#new', as: :new_user_registration
+    get 'signup' => 'devise/registrations#index', as: :new_user_information
+    get 'login' => 'devise/sessions#new', as: :new_user_session
+    post 'login' => 'devise/sessions#create', as: :user_registration
+  end
+  
+
   root to: 'home#index'
 end
