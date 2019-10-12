5c6de008dcca6f64baf661ae2979b2ee04dce12a
HealMon <hey.day.lip@gmail.com>
2019-10-12 15:52:06 +0900

    delete comand out2

--- config/routes.rb
+++ config/routes.rb
@@ -2,22 +2,13 @@ Rails.application.routes.draw do
   
   devise_for :users, skip: :all
   devise_scope :user do
-  #   get 'signup/registration/' => 'devise/registrations#new', as: :new_user_registration
     get 'signup' => 'devise/registrations#index', as: :new_user_information
     get 'login' => 'devise/sessions#new', as: :new_user_session
     post 'login' => 'devise/sessions#create', as: :user_registration
   end
-  
-  # # 仮置場
-  # get 'signup/registration/phone1' => 'home#phone1'
-  # get 'signup/registration/phone2' => 'home#phone2'
-  # get 'signup/registration/address' => 'home#address'
-  # get 'signup/registration/payment' => 'home#payment'
-  # get 'signup/registration/congrats' => 'home#congrats'
 
   resources :signup do
     collection do
-      # get 'signup/registration/' => 'devise/registrations#new', as: :new_user_registration
       get 'registration/' => 'signup#new'
       get 'sms_confirmation/' => 'signup#phone1'
       get 'sms_confirmation/sms' => 'signup#phone2'
