acf970fda833742b876792024c8ebbe1bc5d1e6d
message2tomorrow <cosmic.birth@gmail.com>
2019-11-07 20:43:36 +0900

    item削除機能の再編集

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -45,7 +45,6 @@ class ItemsController < ApplicationController
   end
   
   def destroy
-    @item = Item.find(params[:id])
     if user_signed_in? && current_user.id == @item.user_id
       @item.destroy
       redirect_to root_path
@@ -98,6 +97,10 @@ class ItemsController < ApplicationController
 
   
   private
+  def set_item
+    @item = Item.find(params[:id])
+  end
+
   def item_params
     params.require(:item).permit(
                             :name,
