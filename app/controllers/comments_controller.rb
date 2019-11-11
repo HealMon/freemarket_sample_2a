717728c5d0c63947e1e9cd4102fcc5c12351ff42
HealMon <hey.day.lip@gmail.com>
2019-11-11 19:12:58 +0900

    コメントflash

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,6 +1,7 @@
 class CommentsController < ApplicationController
   def create
     Comment.create(content: comment_params[:content], user_id: current_user.id, item_id: params[:id])
+    flash[:success] = 'コメントを作成しました'
     redirect_to item_path
   end
   
