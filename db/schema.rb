4739108c8c7c92961402156ea295fe7915800d79
message2tomorrow <cosmic.birth@gmail.com>
2019-11-11 21:04:44 +0900

    tosabe-editted(土佐弁編集)

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_05_062621) do
+ActiveRecord::Schema.define(version: 2019_10_23_023002) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -57,16 +57,6 @@ ActiveRecord::Schema.define(version: 2019_11_05_062621) do
     t.datetime "updated_at", null: false
     t.index ["category_id"], name: "index_category_sizes_on_category_id"
     t.index ["products_size_id"], name: "index_category_sizes_on_products_size_id"
-  end
-
-  create_table "comments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.bigint "item_id"
-    t.bigint "user_id"
-    t.text "content", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["item_id"], name: "index_comments_on_item_id"
-    t.index ["user_id"], name: "index_comments_on_user_id"
   end
 
   create_table "items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
@@ -137,6 +127,4 @@ ActiveRecord::Schema.define(version: 2019_11_05_062621) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "cards", "users"
-  add_foreign_key "comments", "items"
-  add_foreign_key "comments", "users"
 end
