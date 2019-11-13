652a6a8591fd36febc4439cfa971d5ebd312757f
HealMon <hey.day.lip@gmail.com>
2019-11-13 14:28:15 +0900

    Update 20191017022459_create_items.rb

--- db/migrate/20191017022459_create_items.rb
+++ db/migrate/20191017022459_create_items.rb
@@ -2,7 +2,7 @@ class CreateItems < ActiveRecord::Migration[5.2]
   def change
     create_table :items do |t|
       t.string :name,                     null: false
-      t.string :description,              null: false
+      t.text :description,                null: false
       t.integer :price,                   null: false
       t.integer :condition,               null: false
       t.integer :shipping_charge_id,      null: false
